a673f5b9a2f17e1e7c65fe44bb80dd7e5bb09334
Yanda Huang <ward11sdo@qq.com>
2017-02-04 21:06:46 -0600

    No error now. However, both data and n is weird

--- electronApp/test.js
+++ electronApp/test.js
@@ -20,7 +20,7 @@ var UrgLibrary = ffi.Library('./liburg_c', {
 
 var urg = ref.alloc(urgPtr);
 var time_stamp = ref.alloc('long')
-var max_distance = ref.alloc('long')
+var min_distance = ref.alloc('long')
 var max_distance = ref.alloc('long')
 var device = ref.allocCString('/dev/ttyS1');
 var res = UrgLibrary.urg_open(urg, 0, device, 115200);
@@ -30,23 +30,22 @@ if (res<0){
     console.log('Successfully connect to the serial port')
 }
 
-// var data = new longArray(UrgLibrary.urg_max_data_size(urg))
-//
-// UrgLibrary.urg_start_measurement(urg, 0, 1, 0)
-//
-// var n = UrgLibrary.urg_get_distance(urg, data, time_stamp)
-// if (n<0) {
-//     console.error('Error')
-//     UrgLibrary.urg_close(urg)
-// }
-// console.log(data)
-// UrgLibrary.urg_distance_min_max(urg, min_distance, max_distance)
-// for (var i = 0; i<n; ++i) {
-//     let distance = data[i]
-//     if ((distance < min_distance) || (distance > max_distance)) {
-//         continue;
-//     }
-//     let radian = UrgLibrary.urg_index2rad(urg, i)
-//     console.log(radian)
-// }
-// UrgLibrary.close(urg)
+var data = new longArray(UrgLibrary.urg_max_data_size(urg))
+UrgLibrary.urg_start_measurement(urg, 0, 1, 0)
+
+var n = UrgLibrary.urg_get_distance(urg, data, time_stamp)
+console.log(data[0], data[1], data[2])
+if (n<0) {
+    console.error('Error when getting distance')
+    UrgLibrary.urg_close(urg)
+}
+UrgLibrary.urg_distance_min_max(urg, min_distance, max_distance)
+for (var i = 0; i<n; ++i) {
+    let distance = data[i]
+    if ((distance < min_distance) || (distance > max_distance)) {
+        continue;
+    }
+    let radian = UrgLibrary.urg_index2rad(urg, i)
+    console.log(distance, radian)
+}
+UrgLibrary.urg_close(urg);
