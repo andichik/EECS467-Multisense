9ec735e04e7948d9244aefdb42e3b4f20fc3afd0
Russell Ladd <russell.ladd@grl5.com>
2017-02-06 14:08:49 -0500

    Remote initiates laser scanning on connection

--- MayApp/MayApp/LaserController.swift
+++ MayApp/MayApp/LaserController.swift
@@ -15,9 +15,14 @@ final class LaserController {
     let sensorPort = "/dev/tty.usbmodemFA131" // Russell
     //let sensorPort = "/dev/cu.usbmodem1421" // Yulin
     
+    var timer: Timer? {
+        willSet {
+            timer?.invalidate()
+        }
+    }
+    
     init() {
         
-        urg_open(&urg, URG_SERIAL, sensorPort, 115200)
     }
     
     func measure() -> [Int] {
@@ -40,5 +45,43 @@ final class LaserController {
             
             print(distance)
         }*/
+    }
+    
+    func measureContinuously(_ block: @escaping ([Int]) -> Void) {
+        
+        print("LASER ON")
+        urg_open(&urg, URG_SERIAL, sensorPort, 115200)
+        urg_start_measurement(&urg, URG_DISTANCE, Int32(URG_SCAN_INFINITY), 0)
+        
+        var distances = Array<Int>(repeating: 0, count: Int(urg_max_data_size(&urg)))
+        
+        let scanTime = TimeInterval(urg_scan_usec(&urg)) / 1.0E6
+        
+        if #available(OSX 10.12, *) {
+            
+            timer = Timer.scheduledTimer(withTimeInterval: scanTime, repeats: true) { [unowned self] timer in
+                
+                let sampleCount = Int(urg_get_distance(&self.urg, &distances, nil))
+                
+                guard sampleCount > 0 else {
+                    print("LASER ERROR: \(sampleCount)")
+                    return
+                }
+                
+                block(Array<Int>(distances.prefix(upTo: sampleCount)))
+            }
+            
+        } else {
+            
+            // Yulin update your computer please :)
+        }
+    }
+    
+    func stopMeasuring() {
+        
+        print("LASER OFF")
+        urg_close(&urg)
+        
+        timer = nil
     }
 }
