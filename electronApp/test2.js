5d815305053eba463f8680e7a1ba7da1befabc71
Yanda Huang <ward11sdo@qq.com>
2017-02-06 15:47:34 -0600

    Finished everything except UI

--- electronApp/test2.js
+++
@@ -1,9 +0,0 @@
-const execFile = require('child_process').execFile
-
-var child = execFile("./calculate_xy", [],
-  function (error, stdout, stderr) {
-    // This callback is invoked once the child terminates
-    // You'd want to check err/stderr as well!
-    console.log("Here is the complete output of the program: ");
-    console.log(stdout)
-});
