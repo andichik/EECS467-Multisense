08ac510e6f42c1965b98df794a93fcfa1f84a12a
Yanda Huang <ward11sdo@qq.com>
2017-02-08 14:40:51 -0600

    A server using websocket

---
+++ NodeServer/public/javascript/index.js
@@ -0,0 +1,39 @@
+var socket = io('http://localhost');
+$('#connect').click(()=>{
+    socket.emit('connectPort', {
+        ArduinoPortName: $('#arduinoPort').val(),
+        LaserPortName: $('#laserPort').val()
+    })
+})
+
+$('#setSpeed').click(()=>{
+    socket.emit('setSpeed', {
+        left: $('#leftSpeed').val(),
+        right: $('#rightSpeed').val()
+    })
+})
+$('#stop').click(()=>socket.emit('stop'))
+
+$('#laser_map').click(()=>socket.emit('showMap'))
+
+socket.on('encoderStr', str=>{
+    var leftExp = /\d+(?=l)/;
+    var rightExp = /\d+(?=r)/;
+    $('#decoder_l').text('Left encoder: '+str.match(leftExp))
+    $('#decoder_r').text('Right encoder: '+str.match(rightExp))
+})
+
+var laserMap = SVG('laser').size(500, 500)
+
+var laserData=[];
+
+socket.on('laserData', (laser_d)=>laserData=laser_d)
+
+var polyline = {remove:()=>{}};
+function drawMap(){
+    polyline.remove();
+    polyline = laserMap.polyline(laserData).fill('none').stroke({ width: 1 })
+    requestAnimationFrame(drawMap)
+}
+
+drawMap()
---
+++ NodeServer/server.js
@@ -0,0 +1,81 @@
+// This file is required by the index.html file and will
+// be executed in the renderer process for that window.
+// All of the Node.js APIs are available in this process.
+
+const SerialPort = require('serialport');
+const Laser = require('./laser.js');
+const SVG = require('svg.js')
+const $ = require("jquery");
+
+var express = require('express')
+var app = require('express')();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+server.listen(80);
+
+console.log('Server started')
+
+app.use(express.static('public'))
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+
+var ArduinoPort, LaserPortName, laserMap;
+
+io.on('connection', function (socket) {
+    socket.on('connectPort', ({ArduinoPortName, LaserPortName})=>{
+        var Readline = SerialPort.parsers.Readline;
+        ArduinoPort = new SerialPort(ArduinoPortName, {
+          baudRate: 9600
+        });
+        var parser = new Readline();
+        ArduinoPort.pipe(parser);
+        parser.on('data', str=>socket.emit('encoderStr', str));
+        console.log("try to connect")
+    })
+    socket.on('setSpeed', ({left, right})=>setSpeed(left, right))
+    socket.on('stop', ()=>setSpeed(0, 0))
+
+    setImmediate(()=>{
+        socket.emit('laserData', Laser.getXY(LaserPortName, 18, 100, 250))
+    })
+
+});
+
+function setSpeed(left, right){
+    ArduinoPort.write(`${left}l${right}r`);
+    console.log(`${left}l${right}r`)
+}
+/*
+document.getElementById('connect').onclick = ()=>{
+    const ArduinoPortName = document.getElementById('arduinoPort').value;
+    const LaserPortName = document.getElementById('laserPort').value;
+
+    function setSpeed(left, right){
+        ArduinoPort.write(`${left}l${right}r`);
+        console.log(`${left}l${right}r`)
+    }
+
+    document.getElementById('setSpeed').onclick=()=>{
+        setSpeed(document.getElementById('leftSpeed').value, document.getElementById('rightSpeed').value)
+    }
+
+    document.getElementById('stop').onclick=()=>{
+        setSpeed(0,0);
+    }
+
+    var polyline = {remove:()=>{}};
+    function readLaser(){
+        laserData = Laser.getXY(LaserPortName, 18, 100, 250);
+        //console.log(laserData)
+        polyline.remove();
+        polyline = laserMap.polyline(laserData).fill('none').stroke({ width: 1 })
+        requestAnimationFrame(readLaser)
+    }
+
+    var laserMap = SVG('laser').size(500, 500)
+    //readLaser()
+}
+*/
