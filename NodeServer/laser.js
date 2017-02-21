6ae5f4648a56a6e97fe6df2194d49f5ba7e21d73
Yanda Huang <ward11sdo@qq.com>
2017-02-21 16:25:05 -0600

    Halfway grid and some format issues

--- NodeServer/laser.js
+++ NodeServer/laser.js
@@ -5,12 +5,12 @@ const ArrayType = require('ref-array');
 
 const MM_TO_M = 0.001;
 
-var getXY = function(portName){
+var getXY = function(portName) {
     var longArray = ArrayType(ref.types.long);
     var longPtr = ref.refType(ref.types.long);
 
     var UrgLibrary = ffi.Library('./liburg_c', {
-      "urg_calculate_xy": [ 'int', [ longArray, longArray, 'CString' ] ],
+        "urg_calculate_xy": ['int', [longArray, longArray, 'CString']],
     });
 
     var x_arr = new longArray(1081)
@@ -26,8 +26,10 @@ var getXY = function(portName){
     var xArr = Array.from(x_arr);
     var yArr = Array.from(y_arr);
 
-    return xArr.map((v_x, idx)=>[v_x*MM_TO_M, yArr[idx]*MM_TO_M])
+    return xArr.map((v_x, idx) => [v_x * MM_TO_M, yArr[idx] * MM_TO_M])
 
 }
 
-module.exports = {getXY}
+module.exports = {
+    getXY
+}
--- NodeServer/public/javascript/const.js
+++ NodeServer/public/javascript/const.js
@@ -11,11 +11,19 @@ const TRACE_HEIGHT = 400;
 const TRACE_WIDTH = 400;
 const TRACE_SCALE = 80;
 
+//Map stuff
+const GRIDS_IN_METER = 400;
+const MAP_LENGTH = 20;
+const GRID_LENGTH = 1 / GRIDS_IN_METER;
+const GRIDS_ON_SIDE = GRIDS_IN_METER * MAP_LENGTH;
+
 export {
-        POSE_UPDATE_SIZE,
-        BASELINE,
-        TICK_STEP,
-        TRACE_HEIGHT,
-        TRACE_WIDTH,
-        TRACE_SCALE
-    };
+    POSE_UPDATE_SIZE,
+    BASELINE,
+    TICK_STEP,
+    TRACE_HEIGHT,
+    TRACE_WIDTH,
+    TRACE_SCALE,
+    GRIDS_ON_SIDE,
+    GRID_LENGTH
+};
