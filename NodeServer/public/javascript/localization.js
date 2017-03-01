1e43b06a9d74b86b351d54383d923d101458a9ec
kevrab <kevrab@umich.edu>
2017-03-01 13:12:30 -0500

    Updated pose.js for Action Error Model, implemented Action Error Model inside localization

---
+++ NodeServer/public/javascript/localization.js
@@ -0,0 +1,14 @@
+import gaussian from 'gaussian'
+
+// Action Model
+//-------------------------
+// INPUTS
+// 	particles - Array of N possible particles of pose class
+// 	leftEnc - Raw left encoder value
+//	rightEnc - Raw right encoder value
+
+function action_model(particles,leftEnc,rightEnc){
+	for (i = 0;i < particles.length;i++){
+		particles[i].update(leftEnc,rightEnc);
+	}	
+}
--- NodeServer/public/javascript/pose.js
+++ NodeServer/public/javascript/pose.js
@@ -4,6 +4,7 @@ import {
     TICK_STEP
 } from './const.js';
 import math from 'mathjs';
+import gaussian from 'gaussian';
 
 class Pose {
     constructor() {
@@ -17,10 +18,23 @@ class Pose {
         else {
             let d_l = (leftEnc - this.leftOld) * TICK_STEP;
             let d_r = (rightEnc - this.rightOld) * TICK_STEP;
-            let delta_x = (d_r + d_l) / 2;
+            let delta_s = (d_r + d_l) / 2;
             let delta_theta = (d_r - d_l) / BASELINE;
             let theta = this.pos[2];
-            this.pos = math.add(this.pos, [delta_x * Math.cos(theta), delta_x * Math.sin(theta), delta_theta]);
+	    	let delta_x = delta_s + cos(theta) - pos[0];
+	    	let delta_y = delta_s + sin(theta) - pos[1];
+	    	// Accounting for delta_x = 0
+	    	if (delta_x === 0) delta_x = 0.0001;
+
+	    	// Setting error terms for Action Error Model
+	    	e1 = gaussian(0,k1*alpha);
+	    	e2 = gaussian(0,k2*delta_s);
+	    	e3 = gaussian(0,k1*(delta_theta-alpha));
+
+	    	let alpha = Math.atan2(delta_y,delta_x) - delta_theta;
+
+			// Calculating new position for particle dispersion using error terms
+            let pos = math.add(this.pos, [(delta_s+e2) * Math.cos(theta+alpha+e1), (delta_s+e2) * Math.sin(theta+alpha+e1), delta_theta+e1+e3]);
             this.leftOld = leftEnc;
             this.rightOld = rightEnc;
         }
