918fa9032decda448db7b2c9cacc95b6fbf916b4
kevrab <kevrab@umich.edu>
2017-03-01 17:34:01 -0500

    Fixed return statement on particle filter, part way done with sensor model.  Need to finish normalizing weights in sensor model.

--- NodeServer/public/javascript/localization.js
+++ NodeServer/public/javascript/localization.js
@@ -32,7 +32,7 @@ function particle_filter(particles){
 		delete particle[i];
 
 	// Set old particles reference to new particles
-	particles = newParticles;
+	return newParticles;
 }
 
 
@@ -64,6 +64,46 @@ function action_model(particles,leftEnc,rightEnc){
 //	map - 2D Arrray of occupancy grid
 
 function sensor_model(particles, laser, map){
-	
+    let log_prob_total = 0;
+
+    // Calculating log probabilities for all particles
+    for (let j = 0; j < particles.length;j++){
+
+        // Log probability total for particle
+        let log_prob_particle = 0;
+
+	    for (let i = 0; i < laserData.length;i++) {
+            
+		    // Calculating pixel coordinates and pixels inbetween
+		    px_calc = calculatePixelPositions(particle, laserData,PX);
+
+            // Setting log probability for case where obstacle is past laser
+            log_prob = -12;
+
+            // Looping through between particle and laser to find obstacle
+		    for (var j = 0; j < px_calc.points_btwn.length; j++) {
+		        let {x, y} = px_calc.points_btwn[j];
+
+			    // Check if grid pixel is occupied
+		        if (mapData[x][y] >= 0.5)
+                    // Checking if occupied pixel is laser pixel
+			        if (j === px_calc.points_btwn.length-1)
+				        log_prob = -4;
+                        break;
+                    // If not, we know there is object before laser point
+			        else
+				        log_prob = -8;
+                        break;
+		    }
+            log_prob_particle += log_prob;
+	    }
+        particle[j].weight = log_prob_particle;
+        log_prob_total += log_prob_particle;
+    }
+
+    // Normalizing log probabilities
+    // Not correct, need to look into underflow problem
+    //for (i=0;i<particles.length;i++){
+    //    particle[i].weight /= log_prob_total;
 
 }
--- NodeServer/public/javascript/map.js
+++ NodeServer/public/javascript/map.js
@@ -2,6 +2,44 @@ import {OCCUPY_REWARD, UNOCCUPY_REWARD, DISPX} from './const.js'
 import math from 'mathjs'
 import bresenham from 'bresenham'
 
+
+// calculatePixelPositions
+// DESCRIPTION
+//      Takes in particle, laser data, and pixel constants, and
+//      generates the particle location in pixels as well as the
+//      pixels bewteen the laser and the particle
+// INPUTS
+//      particle - Specific particle of class particle
+//      laserData - 2D array of x,y coordinates for laser data
+//      PX - Object containing useful length->pixel conversions
+// OUTPUTS
+//      px_x - Location of particle in x direction in pixels
+//      px_y - Location of particle in y direction in pixels
+//      points_btwen - 2D array of x,y coordinates for pixels between
+//                     laser and particle
+
+function calculatePixelPositions(particle,laserData,PX){
+    let world_x = laserData[i][0] + particle.pos[0];
+    let world_y = laserData[i][1] + particle.pos[1];
+
+    let px_x = math.floor(PX.MAP_LENGTH_PX / 2 + world_x / PX.PX_LENGTH_METER);
+    let px_y = math.floor(PX.MAP_LENGTH_PX / 2 + world_y / PX.PX_LENGTH_METER);
+
+    if (!(px_x >= 0 && px_x < PX.MAP_LENGTH_PX &&
+        px_y >= 0 && px_y < PX.MAP_LENGTH_PX)) {
+        continue;
+    }
+
+    let map_pos = particle.mapPos(PX);
+    let points_btwn = bresenham(map_pos[0], map_pos[1], px_x, px_y);
+
+    return  {
+        px_x:px_x,
+        px_y:px_y,
+        points_btwen:points_btwen
+    };
+}
+
 /**
  * Using laser data, update Odd counts in the map
  * @param  {Particle} particle      Estimated pose
@@ -10,7 +48,7 @@ import bresenham from 'bresenham'
  * @param  {Object} PX        Constant ibjct contanining necessary constants
  * @return {Object}           Boundary of changed data
  */
-function updateMapData(paticle, mapData, laserData, PX){
+function updateMapData(particle, mapData, laserData, PX){
 
     var max_x = 0;
     var max_y = 0;
@@ -18,28 +56,19 @@ function updateMapData(paticle, mapData, laserData, PX){
     var min_y = Infinity;
 
     for (let i = 0; i < laserData.length;i++) {
-        let world_x = laserData[i][0] + particle.pos[0];
-        let world_y = laserData[i][1] + particle.pos[1];
+        
+        // Calculating pixel coordinates and pixels inbetween
+        px_calc = calculatePixelPositions(particle, laserData,PX);
 
-        let px_x = math.floor(PX.MAP_LENGTH_PX / 2 + world_x / PX.PX_LENGTH_METER);
-        let px_y = math.floor(PX.MAP_LENGTH_PX / 2 + world_y / PX.PX_LENGTH_METER);
-
-        if (!(px_x >= 0 && px_x < PX.MAP_LENGTH_PX &&
-            px_y >= 0 && px_y < PX.MAP_LENGTH_PX)) {
-            continue;
-        }
         if (PX===DISPX){
-            min_x = math.min(min_x, px_x);
-            min_y = math.min(min_y, px_y);
-            max_x = math.max(max_x, px_x);
-            max_y = math.max(max_y, px_y);
+            min_x = math.min(min_x, px_calc.px_x);
+            min_y = math.min(min_y, px_calc.px_y);
+            max_x = math.max(max_x, px_calc.px_x);
+            max_y = math.max(max_y, px_calc.px_y);
         }
-
         mapData[px_x][px_y] += OCCUPY_REWARD; //Change to const
-        let map_pos = particle.mapPos(PX);
-        let points_btwn = bresenham(map_pos[0], map_pos[1], px_x, px_y);
-        for (var j = 0; j < points_btwn.length; j++) {
-            let {x, y} = points_btwn[j];
+        for (var j = 0; j < px_calc.points_btwn.length; j++) {
+            let {x, y} = px_calc.points_btwn[j];
             mapData[x][y]-= UNOCCUPY_REWARD;
         }
     }
