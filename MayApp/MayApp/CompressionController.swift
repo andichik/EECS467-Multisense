6b71151942459140a0447ed4375769a3201fc866
Russell Ladd <grladd@umich.edu>
2017-04-20 15:43:27 -0400

    Started implementing h264 compression and decompression but gave up because too hard and not enough time for now. A bunch of resources are listed in CompressionController.swift so I can pick this up again in the future.

---
+++ MayApp/MayApp/CompressionController.swift
@@ -0,0 +1,9 @@
+//
+//  CompressionController.swift
+//  MayApp
+//
+//  Created by Russell Ladd on 4/18/17.
+//  Copyright © 2017 University of Michigan. All rights reserved.
+//
+
+import Foundation
